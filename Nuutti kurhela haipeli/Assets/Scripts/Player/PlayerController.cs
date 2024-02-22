d8c0114331bd7220b686015cd29c15d64c64b37e
BeemsChurger67 <toivo.daniel.harju@gmail.com>
2024-02-22 17:23:48 +0200

    neliö

---
+++ Nuutti kurhela haipeli/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed=5f;
+
+    private Master controls;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
