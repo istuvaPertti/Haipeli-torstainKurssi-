0b33776149f1596a476ffa95011aaf1c6bb85005
BeemsChurger67 <toivo.daniel.harju@gmail.com>
2024-02-22 18:15:43 +0200

    liikukwdk

--- Nuutti kurhela haipeli/Assets/Scripts/Player/PlayerController.cs
+++ Nuutti kurhela haipeli/Assets/Scripts/Player/PlayerController.cs
@@ -5,17 +5,27 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed=5f;
-
+private Vector2 moveInput;
     private Master controls;
+    private Rigidbody2D body;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        controls=new Master();
+        body = GetComponent<Rigidbody2D>();
+    }
+    private void onEnable(){
+        controls.Enable();
+    }
+    private void onDisable(){
+        controls.Disable();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        moveInput = controls.Player.Move.ReadValue<Vector2>();
+        Vector2 movement= new Vector2(moveInput.x,moveInput.y)*moveSpeed * Time.fixedDeltaTime;
+        body.MovePosition(body.position+movement);
     }
 }
